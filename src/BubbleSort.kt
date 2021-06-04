b7ad390d7e77805698d1f5664d69b4fe7a88ed18
YSCode <ys.hazard@gmail.com>
2021-06-04 16:22:29 +0900

    버블 정렬 주석 추가 및 기타 수정

--- src/BubbleSort.kt
+++ src/BubbleSort.kt
@@ -1,11 +1,13 @@
 import java.util.ArrayList
 
 /**
- * 버블 정렬
+ * 버블 정렬(Bubble Sort)
+ * 정렬되는 모습이 거품과 비슷해서 붙여진 이름.
+ * 정렬시간 : O(n^2)
  */
 fun main() {
     //정렬이 필요한 배열
-    var arrNum = arrayListOf(5, 2, 3, 4, 1)
+    val arrNum = arrayListOf(5, 2, 3, 4, 1)
 
     print("정렬되기 전 상태 :")
     for (beforeSort in arrNum) {
@@ -24,10 +26,10 @@ fun main() {
 //오름차순으로 정렬
 fun sort(arrNum: ArrayList<Int>) {
     var temp = 0
-    for (i in arrNum) {
+    for (i in arrNum) {     //배열 크기 만큼 반복
         for (j in 0 until arrNum.size-1) {
-            if (arrNum[j] > arrNum[j+1]) {
-                temp = arrNum[j]
+            if (arrNum[j] > arrNum[j+1]) {      //0>1, 1>2, 2>3, 3>4 를 비교.
+                temp = arrNum[j]                //자리바꿈
                 arrNum[j] = arrNum[j+1]
                 arrNum[j+1] = temp
             }
---
+++ src/SelectionSort.kt
@@ -0,0 +1,7 @@
+/**
+ * 선택 정렬(Selection Sort)
+ *
+ */
+fun main() {
+    val arrNums = arrayListOf(10, 50, 23, 12, 61)
+}
